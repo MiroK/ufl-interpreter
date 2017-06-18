dbdd13d44b532146ae9a5a8fa8922da83d0b8d2e
mirok <miroslav.kuchta@gmail.com>
2017-06-18 20:41:13 +0200

    update

--- iufl.py
+++ iufl.py
@@ -2,6 +2,8 @@ import numpy as np
 import math
 import dolfin
 import ufl
+from ufl.conditional import LE, GE, LT, GT, EQ, NE
+
 
 # Representation of ufl nodes that are MathFunctions
 # FIXME: Bessel functions are missing
@@ -40,6 +42,12 @@ def expr_eval(expression):
     if isinstance(expression, ufl.constantvalue.Identity):
         return lambda x: np.eye(expression.ufl_shape[0])
 
+    if isinstance(expression, ufl.constantvalue.Zero):
+        return lambda x: np.zeros(expression.ufl_shape)
+
+    if isinstance(expression, ufl.geometry.SpatialCoordinate): # Idenity
+        return lambda x: np.array(x) if not isinstance(x, np.ndarray) else x
+
     ######################################################################################
     # Algebra
     ######################################################################################
@@ -76,6 +84,16 @@ def expr_eval(expression):
         # Tensors
         else:
             return lambda x: np.inner(expr_eval(first)(x), expr_eval(second)(x))
+
+    if isinstance(expression, ufl.tensoralgebra.Dot):
+        args = expression.ufl_operands
+        first, second = args[0], args[1]
+        # Scalars
+        if first.ufl_shape == ():
+            return lambda x: expr_eval(first)(x) * expr_eval(second)(x)
+        # Tensors
+        else:
+            return lambda x: np.dot(expr_eval(first)(x), expr_eval(second)(x))
 
     if isinstance(expression, ufl.tensoralgebra.Cross):
         args = expression.ufl_operands
@@ -137,6 +155,67 @@ def expr_eval(expression):
 
         return lambda x: (lambda A: np.det(A)*np.inv(A))(expr_eval(first)(x))
 
+    ######################################################################################
+    # Conditionals
+    ######################################################################################
+    if isinstance(expression, EQ):
+        args = expression.ufl_operands
+        first, second = args
+
+        return lambda x: expr_eval(first)(x) == expr_eval(second)(x)
+
+    if isinstance(expression, NE):
+        args = expression.ufl_operands
+        first, second = args
+
+        return lambda x: expr_eval(first)(x) != expr_eval(second)(x)
+
+    if isinstance(expression, GT):
+        args = expression.ufl_operands
+        first, second = args
+
+        return lambda x: expr_eval(first)(x) > expr_eval(second)(x)
+
+    if isinstance(expression, LT):
+        args = expression.ufl_operands
+        first, second = args
+
+        return lambda x: expr_eval(first)(x) < expr_eval(second)(x)
+
+    if isinstance(expression, GE):
+        args = expression.ufl_operands
+        first, second = args
+
+        return lambda x: expr_eval(first)(x) >= expr_eval(second)(x)
+
+    if isinstance(expression, LE):
+        args = expression.ufl_operands
+        first, second = args
+
+        return lambda x: expr_eval(first)(x) <= expr_eval(second)(x)
+
+    if isinstance(expression, ufl.operators.AndCondition):
+        args = expression.ufl_operands
+        first, second = args
+
+        return lambda x: expr_eval(first)(x) and expr_eval(second)(x)
+
+    if isinstance(expression, ufl.operators.OrCondition):
+        args = expression.ufl_operands
+        first, second = args
+
+        return lambda x: expr_eval(first)(x) or expr_eval(second)(x)
+
+    if isinstance(expression, ufl.operators.NotCondition):
+        arg = expression.ufl_operands[0]
+
+        return lambda x: not(expr_eval(first)(x))
+
+    if isinstance(expression, ufl.operators.Conditional):
+        cond, true_v, false_v = expression.ufl_operands
+
+        return lambda x: expr_eval(true_v)(x) if expr_eval(cond)(x) else expr_eval(false_v)(x)
+
     assert False
 
 # ----------------------------------------------------------------------------------------
@@ -155,18 +234,15 @@ if __name__ == '__main__':
     B = Constant(((2, 0), (2, 3)))
     v = Constant((1, 0))
 
-    f = expr_eval(det(inv(sym(A)+skew(B)+outer(v, v))))
+    f = expr_eval(det(inv(sym(A)+skew(B)+outer(v, v)+dot(A, B))))
+    print f((0.2, 0.3))
+    f = expr_eval(conditional(gt(1, 2), dot(A, B)+Identity(2), A))
     print f((0.2, 0.3))
 
-    # foo = expr_eval(-sin(u+2*f*cos(v)))
-    # print foo([0.2, 0.3])
-
-    # FIXME: how to handle tensor-valued functions - det, eig, cofac, ...
-    # FIXME: complete functions ...
+    # FIXME: as_vector, as_matrix, Indexed
     # FIXME: things such as CellVolume
 
-    # FIXME: derivatives?
-    # FIXME: wrapping as Expression, eval_cell?
-    # FIXME: cpp Expression
+    # FIXME: differentiation
+    # FIXME: wrapping as Expression, eval_cell?, cpp Expression
 
     # READ SICC: interpreters
